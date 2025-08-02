7f0ba99182bdb71e7ba4d1ef9a59518f1129e695
frantastico <fomartinmail@gmail.com>
2025-08-02 14:28:29 -0700

    Revert "Merge branch 'main' of https://github.com/JustTeysa/GMTK2025"
    This reverts commit 1dcf2b673f67bac139b2eed70fece37177491468, reversing
    changes made to d0b8f41aa3f9960cf3bd85a18940ce15d89c93a0.

--- gmtk2025_loop/entities/npcs/NPC.gd
+++ gmtk2025_loop/entities/npcs/NPC.gd
@@ -1,93 +1,54 @@
 extends Area2D
 
-var speechBubble: SpeechBuble
+@export var speechBubble: SpeechBuble
 @export var Name: String;
 @export var npcGreetingText: String;
-@export var npcItemCompletedText: String;
-#@export var npcEndedText: String;
-@export var isMom: bool = false;
+@export var npcSwapText: String;
+@export var npcEndedText: String;
 
-var ItemWant: String = "NO ITEM WANT DEFINED";
-var ItemHave: String = "NO ITEM WANT DEFINED";
+@export var ItemWant: String = "NO ITEM WANT DEFINED";
+@export var ItemHave: String = "NO ITEM WANT DEFINED";
 
 var swapped: bool = false;
 
 var player: CharacterBody2D
-var contactMade:bool = false;
 
 func _ready():
 	ItemWant.to_upper()
 	ItemHave.to_upper()
-	speechBubble = get_node("/root/Main/CanvasLayer/StoryTimeText")
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		player = body
-		ContactPlayer(player)
-		
-		if (CheckPlayerDialogueFlag(player)):
-			UpdatedTextFields(npcItemCompletedText)
-		else:
-			UpdatedTextFields(npcGreetingText)
-		
-		enableTextBox()
+		#if (swapped)
+			#UpdatedTextFields(npcEndedText)
+		speechBubble.visible = true
+		speechBubble.set_text(Name + ": " + npcGreetingText)
 
 func _on_body_exited(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		player = null
-		disableTextBox()
+		speechBubble.set_text("")
+		speechBubble.visible = false
 
-func enableTextBox():
-	speechBubble.visible = true
-	#speechBubble.set_text(Name + ": " + npcGreetingText)
-	
-func disableTextBox():
-	speechBubble.set_text("")
-	speechBubble.visible = false
-
-func ContactPlayer(player: CharacterBody2D):
-	print("Sending NPC info to Player")
-	
-	if (player == null):
-		return;
-	
-	player.AddContact(Name)
-	contactMade = true;
-
-func CheckPlayerDialogueFlag(player: CharacterBody2D) -> bool:
-	var itemFound = false;
-	print("CheckingPlayerFlag")
-	if (player == null):
-		return false;
-		
-	if (player.Item == "NONE"):
-		print("Item Result: FAILED")
-	
-	if (player.Item == "MOWER"):
-		print("Item Result: SUCCESS")
-		itemFound = true;
-	
-	return itemFound;
-
-func UpdatedTextFields(newText: String):
-	speechBubble.set_text(Name + ": " + newText)
-	
-#func _process(delta):
-#	if player != null and Input.is_action_just_pressed("interact"):
-#		player.Item = "MOWER";
-		#AttemptItemSwap(player)
+func _process(delta):
+	if player != null and Input.is_action_just_pressed("interact"):
+		AttemptItemSwap(player)
 
 func AttemptItemSwap(player: CharacterBody2D) -> bool:
 	print("Attempting to swap " + player.Item + " for " + ItemHave)
+
 	if player.Item == "NONE":
 		print("Result: FAILED")
-		#return false
+		return false
 	
 	var itemName = player.Item
 	player.Item = ItemHave
 	swapped = true;
 	
-	#UpdatedTextFields(npcEndedText)
+	UpdatedTextFields(npcEndedText)
 	print("Result: SUCCESS")
-	print(itemName)
 	return true
+	
+func UpdatedTextFields(newText: String):
+	speechBubble.set_text(Name + ": " + newText)
