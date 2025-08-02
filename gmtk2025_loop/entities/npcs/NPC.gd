b8286b5c40284479029748e82acbc15f1371ec5e
Alination <alinationvarela@gmail.com>
2025-08-01 23:35:34 -0700

    NPCs now have speech bubbles

--- gmtk2025_loop/entities/npcs/NPC.gd
+++ gmtk2025_loop/entities/npcs/NPC.gd
@@ -1,6 +1,6 @@
 extends Area2D
 
-
+@export var speechBubble: SpeechBuble
 
 var player_in_range:bool = false
 
@@ -10,13 +10,12 @@ func _ready():
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		player_in_range = true
-		print("Player entered NPC range.")
-
+		speechBubble.visible = true
 
 func _on_body_exited(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		player_in_range = false
-		print("Player left NPC range.")
+		speechBubble.visible = false
 
 func _process(delta):
 	if player_in_range and Input.is_action_just_pressed("interact"):
--- gmtk2025_loop/scenes/utility/teleport.gd
+++ gmtk2025_loop/scenes/utility/teleport.gd
@@ -2,6 +2,6 @@ extends Area2D
 
 @export var target : Node 	
 
-func _on_body_entered(body: Node2D) -> void:
-	if body.name == "Player":
-		body.global_position = target.global_position
+#func _on_body_entered(body: Node2D) -> void:
+	#if body.name == "Player":
+		#body.global_position = target.global_position
