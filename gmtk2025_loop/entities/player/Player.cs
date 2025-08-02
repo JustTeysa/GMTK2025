2eb2e7ff188d8936130741cfe9e336b5a099393a
Corey Bainard <corey.bainard@gmail.com>
2025-08-02 15:06:29 -0700

    Re-submission of the player and npc changes

--- gmtk2025_loop/entities/player/Player.cs
+++ gmtk2025_loop/entities/player/Player.cs
@@ -1,5 +1,6 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 
 public partial class Player : CharacterBody2D
 {
@@ -23,6 +24,7 @@ public partial class Player : CharacterBody2D
 	private const string moveLeft = "walk_left";
 	private const string interact = "interact";
 	private const string jump = "jump";
+	private const string drop = "down";
 	
 	private bool grounded = false;
 	private int jumpCount = 0;
@@ -49,6 +51,8 @@ public partial class Player : CharacterBody2D
 		defaultYScale = animatedSprite2d.Scale.Y;
 		defaultXScale = animatedSprite2d.Scale.X;
 		maxSquashMagnitude = defaultYScale * maxSquashMagnitude;
+
+		NpcContacts = new Godot.Collections.Dictionary<string, bool>();
 	}
 	
 	public void Start(Vector2 spawnPosition)
@@ -68,6 +72,8 @@ public partial class Player : CharacterBody2D
 	private void Movement(double delta)
 	{
 		Vector2 velocity = Velocity;
+		
+		
 
 		// Add the gravity.
 		if (!IsOnFloor())
@@ -80,6 +86,14 @@ public partial class Player : CharacterBody2D
 		{
 			grounded = true;
 			jumpCount = 0;
+			
+			if(Input.IsActionPressed(drop))
+			{
+				var position = Position;
+				Position = new Vector2(position.X, position.Y + 5);
+				velocity += GetGravity() * (float)delta * GravityMod;
+				grounded = false;
+			}
 		}
 
 		// Handle Jump.
@@ -93,6 +107,7 @@ public partial class Player : CharacterBody2D
 				animatedSprite2d.Play("jump");
 			}
 		}
+		
 
 		// Get the input direction and handle the movement/deceleration.
 		// As good practice, you should replace UI actions with custom gameplay actions.
@@ -186,6 +201,21 @@ public partial class Player : CharacterBody2D
 			{
 				animatedSprite2d.FlipH = true;
 			}
+		}
+	}
+
+	private Godot.Collections.Dictionary<string, bool> NpcContacts;
+	public void AddContact(string npcName)
+	{
+		bool added = NpcContacts.TryAdd(npcName, true);
+
+		if (added)
+		{
+			GD.Print(npcName + ": Contact Success");
+		}
+		else
+		{
+			GD.Print(npcName + ": Contact FAILED");
 		}
 	}
 }
