24bda8335fb5f0752c26142d01bad4b86c5ce330
frantastico <fomartinmail@gmail.com>
2025-08-03 08:23:07 -0700

    Footstep audio in air fix

--- gmtk2025_loop/entities/player/FootstepPlayer.gd
+++ gmtk2025_loop/entities/player/FootstepPlayer.gd
@@ -1,9 +1,13 @@
 extends AudioStreamPlayer2D
 
+@export var Player: CharacterBody2D
 @export var AnimatedSprite: AnimatedSprite2D
 var current_frame = -1;
 
 func _process(deltaTime: float):
+	if !Player.is_on_floor():
+		return
+	
 	if AnimatedSprite.animation.begins_with("walk") && AnimatedSprite.frame % 2 == 1 && current_frame != AnimatedSprite.frame:
 		current_frame = AnimatedSprite.frame
 		play()
