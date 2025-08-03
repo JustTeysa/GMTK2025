bd4d78778937637620f60c0f5f6bb684a7a0d2d1
frantastico <fomartinmail@gmail.com>
2025-08-02 17:59:59 -0700

    Tanooki Easter Eggs

--- gmtk2025_loop/scenes/building/building.gd
+++ gmtk2025_loop/scenes/building/building.gd
@@ -9,8 +9,7 @@ func _ready():
 	for index in range(len(npc_scenes)):
 		var npc_instance = npc_scenes[index].instantiate()
 		var root_index = index % len(npc_roots)
-		npc_roots[root_index]
-		add_child(npc_instance)
+		npc_roots[root_index].add_child(npc_instance)
 		npc_instance.position = Vector2(0,0)
 
 func get_width():
