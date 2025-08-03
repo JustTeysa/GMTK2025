71ee957d9dddd2b2cbb57c5354f907dc55270af6
Corey Bainard <corey.bainard@gmail.com>
2025-08-03 00:58:56 -0700

    No more double audio on jump and lowered player speed and jump for joy reasons

--- gmtk2025_loop/entities/player/FootstepPlayerJump.gd
+++ gmtk2025_loop/entities/player/FootstepPlayerJump.gd
@@ -7,8 +7,7 @@ var has_played_this_jump = false
 func _process(deltaTime: float):
 	if has_played_this_jump && AnimatedSprite.frame == 1:
 		has_played_this_jump = false
-	elif AnimatedSprite.animation.begins_with("jump") && AnimatedSprite.frame == 0:
+	elif AnimatedSprite.animation.begins_with("jump") && AnimatedSprite.frame == 0 && !has_played_this_jump:
 		has_played_this_jump = true
 		play()
-	
 	
