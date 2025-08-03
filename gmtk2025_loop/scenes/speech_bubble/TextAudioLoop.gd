3434c933ccde87474882a6978656c58297fb5fc6
frantastico <fomartinmail@gmail.com>
2025-08-02 22:30:23 -0700

    Modulo text

--- gmtk2025_loop/scenes/speech_bubble/TextAudioLoop.gd
+++ gmtk2025_loop/scenes/speech_bubble/TextAudioLoop.gd
@@ -1,12 +1,14 @@
 extends AudioStreamPlayer2D
 
 @export var label: RichTextLabel
+@export var moduloRange = Vector2(2, 3)
 
 var shortCharCount: int = 0
 var longCharCount: int = 0
 
 var shortLabel: RichTextLabel
 var longLabel: RichTextLabel
+
 
 func _ready() -> void:
 	shortLabel = get_node("/root/Main/ShortSpeech/StoryTimeText/RichTextLabel")
@@ -19,11 +21,13 @@ func _process(deltaTime: float):
 func _processSound_Short(label: RichTextLabel):
 	if shortCharCount != label.visible_characters:
 		shortCharCount = label.visible_characters
-		if shortCharCount > 1 && shortCharCount < label.get_total_character_count():
+		var modulo = randi_range(moduloRange.x, moduloRange.y)
+		if shortCharCount > 1 && shortCharCount < label.get_total_character_count() && shortCharCount % modulo == 0:
 			play()
 	
 func _processSound_Long(label: RichTextLabel):
 	if longCharCount != label.visible_characters:
 		longCharCount = label.visible_characters
-		if longCharCount > 1 && longCharCount < label.get_total_character_count():
+		var modulo = randi_range(moduloRange.x, moduloRange.y)
+		if longCharCount > 1 && longCharCount < label.get_total_character_count() && longCharCount % modulo == 0:
 			play()
